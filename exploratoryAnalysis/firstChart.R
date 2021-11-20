0a8274df65ba39ac25a75e24eae2f95a9e8e9e50
ryanvle <ryanvle@uw.edu>
2021-11-19 18:59:52 -0800

    updated index file with table

--- exploratoryAnalysis/firstChart.R
+++ exploratoryAnalysis/firstChart.R
@@ -4,7 +4,7 @@ national_hate_crimes <- tableSummary %>%
                             religion_crimes = sum(total_religion, na.rm = TRUE),
                             disability_crimes = sum(total_disability, na.rm = TRUE),
                             gender_crimes = sum(total_gender, na.rm = TRUE),
-                            gender_identity_crimes = sum(total_genderIden, na.rm = TRUE)
+                            gender_identity_crimes = sum(total_gender_iden, na.rm = TRUE)
                             )
 type_of_hate_crime <- colnames(national_hate_crimes)
 total <- unlist(national_hate_crimes[1,], use.names = FALSE)
--- exploratoryAnalysis/tableSummary.R
+++ exploratoryAnalysis/tableSummary.R
@@ -1,4 +1,4 @@
-library("tidyverse")
+##library("tidyverse")
 
 crimedata <- read.csv("https://raw.githubusercontent.com/info201b-2021-aut/final-project-tdao09/main/data/Hate_Crime_Incidents_per_Bias_Motivation_and_Quarter_by_State_Federal_and_Agency_2019.csv?token=AV5GX7E45YJD2YCJMH6KGPDBUAYFI")
 
