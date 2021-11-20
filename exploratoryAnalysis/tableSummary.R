2499a1c4f70afba2591e2221d5bae537f96dcd3b
tdao09 <tdao09@uw.edu>
2021-11-19 16:32:08 -0800

    bar chart

--- exploratoryAnalysis/tableSummary.R
+++ exploratoryAnalysis/tableSummary.R
@@ -9,7 +9,7 @@ crimedata <- read.csv("https://raw.githubusercontent.com/info201b-2021-aut/final
 
 ##summary_info
 
-table <- crimedata %>%
+tableSummary <- crimedata %>%
     group_by(State) %>%
     summarise(
       total_race = sum(Race..Ethnicity..Ancestry, na.rm = TRUE),
@@ -17,9 +17,7 @@ table <- crimedata %>%
       total_sex = sum(Sexual.orientation, na.rm = TRUE),
       total_disability = sum(Disability, na.rm = TRUE),
       total_gender = sum(Gender, na.rm = TRUE),
-      total_gender_iden = sum(Gender.identity, na.rm = TRUE), 
-      total_hate_crimes = total_race + total_religion + total_sex + total_disability + total_gender + total_gender_iden
+      total_genderIden = sum(Gender.identity, na.rm = TRUE), 
+      total_HateCrimes = total_race + total_religion + total_sex + total_disability + total_gender + total_genderIden
     
     )
-
-
