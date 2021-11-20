2d26dcc851e0d8cb51a7c5db2e84b7701a292509
EshwarX <69286203+EshwarX@users.noreply.github.com>
2021-11-19 21:30:40 -0800

    Update thirdChart.R

--- exploratoryAnalysis/thirdChart.R
+++ exploratoryAnalysis/thirdChart.R
@@ -1,6 +1,8 @@
 library("knitr")
 library("tidyverse")
 
+crimedata <- read.csv("https://raw.githubusercontent.com/info201b-2021-aut/final-project-tdao09/main/data/Hate_Crime_Incidents_per_Bias_Motivation_and_Quarter_by_State_Federal_and_Agency_2019.csv?token=AV5GX7E45YJD2YCJMH6KGPDBUAYFI")
+
 col=c("slateblue","skyblue","royalblue","steelblue","slategray")
 groups <- c(sum(crimedata$Race..Ethnicity..Ancestry, na.rm = TRUE), sum(crimedata$Sexual.orientation, na.rm = TRUE), sum(crimedata$Disability, na.rm = TRUE), sum(crimedata$Gender, na.rm = TRUE), sum(crimedata$Gender.identity, na.rm = TRUE))
 chem <- c("Race, Ethnicity and Ancestry", "sexual orientation", "disability", "gender","gender identity")
