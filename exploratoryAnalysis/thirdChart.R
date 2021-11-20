a4aa67d846f23f6ba52cef73813a4c0a02a313f1
EshwarX <69286203+EshwarX@users.noreply.github.com>
2021-11-19 21:29:29 -0800

    Update thirdChart.R

---
+++ exploratoryAnalysis/thirdChart.R
@@ -0,0 +1,4 @@
+col=c("slateblue","skyblue","royalblue","steelblue","slategray")
+groups <- c(sum(crimedata$Race..Ethnicity..Ancestry, na.rm = TRUE), sum(crimedata$Sexual.orientation, na.rm = TRUE), sum(crimedata$Disability, na.rm = TRUE), sum(crimedata$Gender, na.rm = TRUE), sum(crimedata$Gender.identity, na.rm = TRUE))
+chem <- c("Race, Ethnicity and Ancestry", "sexual orientation", "disability", "gender","gender identity")
+pie(x=groups, labels=chem, radius=1, main="Proportion of Hate Crimes", col=col, clockwise=T)
