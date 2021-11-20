d246ebf1f537941c5b45ba03ad4637db77aee349
agam0802 <91909116+agam0802@users.noreply.github.com>
2021-11-19 23:19:05 -0800

    Update ScatterPlotChart.R

--- exploratoryAnalysis/ScatterPlotChart.R
+++ exploratoryAnalysis/ScatterPlotChart.R
@@ -12,7 +12,7 @@ state_crimes <- updated_national_hate_crimes%>%
   filter(State == "Washington" & Agency.Type == "Universities and Colleges")
 
 WA_uni_crimes_plot <- ggplot(state_crimes) + 
-  geom_line(mapping = aes(x = Agency +" ", y = sum, group = State, color = Agency)) +
+  geom_line(mapping = aes(x = Agency, y = sum, group = State, color = Agency)) +
   labs(x = "Universities in WA", y = "Num of hate crimes", title = "Hate crimes in Washington Universities")
 WA_uni_crimes_plot <- WA_uni_crimes_plot + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 scatter_plot_hate_crime_chart <- WA_uni_crimes_plot
