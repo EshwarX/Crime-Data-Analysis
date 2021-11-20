f6945fff31df8f21176f14661a258da249bf5983
EshwarX <69286203+EshwarX@users.noreply.github.com>
2021-11-19 21:29:59 -0800

    Update thirdChart.R

--- exploratoryAnalysis/thirdChart.R
+++ exploratoryAnalysis/thirdChart.R
@@ -1,3 +1,6 @@
+library("knitr")
+library("tidyverse")
+
 col=c("slateblue","skyblue","royalblue","steelblue","slategray")
 groups <- c(sum(crimedata$Race..Ethnicity..Ancestry, na.rm = TRUE), sum(crimedata$Sexual.orientation, na.rm = TRUE), sum(crimedata$Disability, na.rm = TRUE), sum(crimedata$Gender, na.rm = TRUE), sum(crimedata$Gender.identity, na.rm = TRUE))
 chem <- c("Race, Ethnicity and Ancestry", "sexual orientation", "disability", "gender","gender identity")
