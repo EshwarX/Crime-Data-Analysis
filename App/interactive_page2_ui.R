337ecefcc3666ea100f30704fc8f737dd3db1da1
EshwarX <69286203+EshwarX@users.noreply.github.com>
2021-12-08 19:45:32 -0800

    Update interactive_page2_ui.R

--- App/interactive_page2_ui.R
+++ App/interactive_page2_ui.R
@@ -1,1 +1,17 @@
-interactive_page2 <- tabPanel("Interactive chart 2")
+interactive_page2 <- tabPanel("Hate Crime By Each Quarter",
+                              # Side
+                              sidebarPanel(
+                                radioButtons(
+                                  inputId = "quarter",
+                                  label = "Select Quarter",
+                                  choices = c("1st Quarter", 
+                                              "2st Quarter",
+                                              "3st Quarter",
+                                              "4st Quarter"),
+                                  selected = "1st Quarter"
+                                )
+                              ),
+                              # Main
+                              mainPanel(
+                                plotlyOutput(outputId = "barChart")
+                              ))
