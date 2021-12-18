3d50d9be917e71cdca4bb7443e535a88381c6e9c
Agampreet <asingh77@uw.edu>
2021-12-17 22:33:03 -0800

    done

---
+++ App/rsconnect.R
@@ -0,0 +1,9 @@
+install.packages('rsconnect')
+
+rsconnect::setAccountInfo(name='agampreet',
+                          token='A30E55545FA7BBAF6531B193332E8504',
+                          secret='2L0WgHwfUvzX/DzmpK7dPbO/uuof0e4I8ZDy06ln')
+
+librabry('rsconnect')
+
+rsconnect::deployApp('/Users/agampreetsingh/assignment-4-data-applications-agam0802')
